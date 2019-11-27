3aed7e6a34cc15605bf935573feffcaf8c8e9cf9
tony <aasun.110@163.com>
2019-11-27 10:35:22 +0800

    第一次提交

---
+++ 22.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#coding=utf-8
+for letter in "long":
+    if letter == "o":
+        continue
+    print("zhi",letter)
+for n in range (2,10):
+    for x in range (2,n):
+        print(n,x)
+for letter in 'Runoob':
+   if letter == 'o':
+      pass
+      print ('执行 pass 块')
+   print ('当前字母 :', letter)
+
+print ("Good bye!")
---
+++ test.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+print("hello,world")
+number=int(input("please input your number:"))
+if number < 18 :
+    print("you are young")
+elif number >=18 :
+    print("hello ,骚年")
+elif number >60 :
+    print("haha")
+input("enter 退出")
+for number in range(100):
+    print(numer)
